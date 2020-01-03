34e8c42f332d8ea08a6b2e81a0d2fdef81e985cc
darling-flag-8189 <dwinniford@gmail.com>
2020-01-03 16:58:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,9 @@ def run_guessing_game
   guess = gets.chomp 
   if guess == num 
     puts "You guessed the correct number!"
-  elsif !(guess == num)
-    puts "Sorry! The computer guessed #{num}."
   elsif guess == "exit"
     puts "Goodbye!"
+  elsif !(guess == num)
+    puts "Sorry! The computer guessed #{num}."
   end 
 end 

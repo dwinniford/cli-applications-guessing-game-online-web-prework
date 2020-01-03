b03edda1f4f617e95d17ebdae79b4c43387cdbc0
darling-flag-8189 <dwinniford@gmail.com>
2020-01-03 16:54:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,11 @@
 def run_guessing_game
   num = rand(1..6)
-  
+  puts "Guess a number between 1 and 6"
+  guess = gets.chomp 
+  if guess == num 
+    puts "You guessed the correct number!"
+  elsif !(guess == num)
+    puts "Sorry! The computer guessed #{num}."
+  elsif guess == "exit"
+    puts "Goodbye!"
 end 
